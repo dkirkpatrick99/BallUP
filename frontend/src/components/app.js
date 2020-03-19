2e1407e688b88b4ae44749b44190f1fd7a854362
CreatorDK <d.kirkpatrick99@gmail.com>
2020-03-19 10:03:27 -0700

    Updated app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,11 +7,13 @@ import NavBarContainer from './nav/navbar_container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container.js';
 // import ProfileContainer from './profile/profile_container';
+import GameShowContainer from './gameShow/game_show_container'
 
 const App = () => (
     <div>
         <NavBarContainer />
         <Switch>
+            <GameShowContainer />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
         </Switch>
