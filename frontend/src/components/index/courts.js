d46ac1b3945785aaf17070d42a27a5b4631225a1
rokylebrown <r.kyle.brown@gmail.com>
2020-03-20 13:09:17 -0700

    updated nav, map on index

--- frontend/src/components/index/courts.js
+++ frontend/src/components/index/courts.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Map, GoogleApiWrapper } from 'google-maps-react';
-// import MapContainer from '../map/map'
+import MapContainer from '../map/map'
 
 class Courts extends React.Component {
     constructor(props) {
@@ -8,19 +8,36 @@ class Courts extends React.Component {
 
     }
 
-    componentWillMount() {
-        // this.props.getGames()
+    componentDidMount(){
+        this.props.getGames()
     }
 
+    // componentWillMount() {
+    //     // this.props.getGames()
+    // }
+
     render() {
+
+        debugger;
+
         return (  
             <div>
                 <h1>Hello</h1>
 
                 <h1>Put the map of courts here</h1>
-                <p>{this.props.courts}</p>
+                {/* <p>{this.props.courts}</p> */}
 
                 <h1>Put the list of filtered games here</h1>
+
+                {this.props.games.map( game => 
+
+                    <ul>
+                        <li>{game.location}</li>
+                        <li>{game.time}</li>
+                    </ul>
+                    
+                    )}
+                    <MapContainer />
             </div>  
         
         )}
