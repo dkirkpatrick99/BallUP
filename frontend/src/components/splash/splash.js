fe8008dd564d8ef037e8726e455c6983d05733c5
rokylebrown <r.kyle.brown@gmail.com>
2020-03-21 22:29:02 -0700

    basic modals

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,10 +1,19 @@
 import '../nav/navbar.css'
 import React from 'react';
-import LoginContainer from '../session/login_form_container'
+import LoginContainer from '../session/login_form_container';
+import SignupContainer from '../session/signup_form_container';
+import $ from 'jquery';
 
 class Splash extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+
+    signupModal(e) {
+        e.preventDefault();
+        $('.su-modal-background-closed').addClass('su-modal-background');
+        $('.su-modal-background-closed').removeClass('su-modal-background-closed');
     }
 
     render(){
@@ -14,14 +23,17 @@ class Splash extends React.Component {
             <div className="hero">
             </div>
             <div className="tag-container">
-                    {/* <div className="modal-background">
+                    <div className="modal-background-closed">
                         <LoginContainer />
-                    </div> */}
+                    </div>
+                    <div className="su-modal-background-closed">
+                        <SignupContainer />
+                    </div>
                 <ul className="tag-list">
                     <li><h1 id="tag">Dope tagline promoting our product</h1></li>
                     <li><p id="sub-tag">Cool stuff describing why folks should join BallUP, and how clicking the button below will change their lives.</p></li>
                 </ul>
-                <button className="signup-btn">Got Next?</button>
+                    <button onClick={this.signupModal} className="signup-btn">Got Next?</button>
             </div>
             </div>
         )
