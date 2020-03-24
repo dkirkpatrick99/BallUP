eb5e9e54dad93eaa76ab69d9bcd0068c98f3de0f
rokylebrown <r.kyle.brown@gmail.com>
2020-03-24 15:20:49 -0700

    heroku try4

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const users = require("./routes/api/users");
 const games = require("./routes/api/games");
 
 mongoose
-  .connect(db, {
+  .connect(process.env.MONGODB_URI || db, {
     useNewUrlParser: true, useUnifiedTopology: true,
     useFindAndModify: false
   })
@@ -32,10 +32,11 @@ app.use("/api/games", games);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
