a859a58fc7c78148ef543de13d8e5e95f4d9a892
rokylebrown <r.kyle.brown@gmail.com>
2020-03-25 11:02:12 -0700

    93

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const path = require('path');
 const users = require("./routes/api/users");
 const games = require("./routes/api/games");
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+
+
 mongoose
   .connect(process.env.MONGODB_URI || db, {
     useNewUrlParser: true, useUnifiedTopology: true,
@@ -33,10 +42,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/',express.static(path.join(__dirname, 'frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
