bcdb39e7764471774b0a4f7cc7d42597037078a6
rokylebrown <r.kyle.brown@gmail.com>
2020-03-25 10:58:58 -0700

    92

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
+  app.use('/',express.static(path.join(__dirname, 'frontend/build')));
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
