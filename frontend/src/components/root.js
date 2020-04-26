8c1396607331bf06a1e83ae1fbc98c35396e0eff
rokylebrown <r.kyle.brown@gmail.com>
2020-04-25 21:34:23 -0700

    scroll test7

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -7,7 +7,18 @@ import { Scrollbars } from 'react-custom-scrollbars';
 const Root = ({ store }) => (
     <Provider store={store}>
         <HashRouter>
-            <Scrollbars style={{ height: "100vh"}}>
+            <Scrollbars 
+                style={{ height: "100vh"}}
+                autoHide
+                autoHideTimeout={1000}
+                autoHideDuration={200}
+                autoHeight
+                autoHeightMin={0}
+                autoHeightMax={200}
+                thumbMinSize={30}
+                universal={true}
+                {...this.props}>
+                >
                 <App />
             </Scrollbars>
         </HashRouter>
