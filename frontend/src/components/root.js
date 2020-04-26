14d81668e733f335ad9021ac8b80224abd180d70
rokylebrown <r.kyle.brown@gmail.com>
2020-04-25 21:36:15 -0700

    scroll test9

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -12,9 +12,6 @@ const Root = ({ store }) => (
                 autoHide
                 autoHideTimeout={1000}
                 autoHideDuration={200}
-                autoHeight
-                autoHeightMin={0}
-                autoHeightMax={200}
                 thumbMinSize={30}
                 universal={true}
                 >
